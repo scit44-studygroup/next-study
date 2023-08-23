3dd78e591053779ccd46a303f236701319728cef
Hwikun <leah8608@gmail.com>
2023-08-23 09:00:37 +0900

    tailwindcss 사용법

---
+++ pages/tailwind.tsx
@@ -0,0 +1,12 @@
+export default function Tailwind() {
+	return (
+		<div className="grid grid-cols-3 gap-4">
+			<div>떡볶이</div>
+			<div>순대</div>
+			<div>라면</div>
+			<div>파스타</div>
+			<div>스시</div>
+			<div>마라탕</div>
+		</div>
+	);
+}
